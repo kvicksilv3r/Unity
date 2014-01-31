de85fd65e8d604e241b10a8897234f9d2fe23949
kvicksilv3r <skink100@hotmail.com>
2014-01-31 16:05:53 +0100

    Synced to git
    UNITEH

---
+++ GraveRunner/Assets/GraveRunnerAssets/Scripts/LifetimeVariables.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifetimeVariables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public int deaths;
+	
+	public int coins;
+	
+	public int plays;
+	
+	public int treecollide;
+	
+	public int eyecollide;
+	
+	public int stonecollide;
+	
+	public int ghostcollide;
+	
+	public float secondsPlayed;
+	
+	public int minutesPlayed;
+
+	public int hoursPlayed;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
