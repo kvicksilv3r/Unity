0849ecd44ae16e02a56e4490f804cbcc27efc0bf
kvicksilv3r <skink100@hotmail.com>
2014-02-07 14:28:34 +0100

    working cells
    Swagger

---
+++ GraveRunner/Assets/CellScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CellScript : MonoBehaviour {
+
+	public float z;
+	public GameObject CellPrefab;
+	public float speed = 0.75f;
+	float despawnPoint;
+	float offset;
+
+	// Use this for initialization
+	void Start () {
+		z = transform.position.z;
+		despawnPoint = -CellPrefab.transform.lossyScale.z * 8;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(new Vector3(0,0, -0.5f));
+
+		z -= CellPrefab.transform.lossyScale.z * 8 * Time.deltaTime * speed;
+		transform.position = new Vector3 (0,0, z);
+
+		if(z <= despawnPoint){
+			//transform.position = new Vector3(0,0, 56);
+			//script1.makeCell();
+			offset = z - despawnPoint;
+			//Destroy(gameObject);
+			z = 4*CellPrefab.transform.lossyScale.z*8 + offset;
+		}
+
+
+	}
+}
---
+++ GraveRunner/Assets/SetupScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupScript : MonoBehaviour {
+
+	public Transform CellPrefab;
+	public Vector3 foobar;
+
+	// Use this for initialization
+	void Start () {	
+		for (int i = 0; i < 5; i++) {
+			Instantiate(CellPrefab, new Vector3(0, 0, i* CellPrefab.renderer.bounds.size.z), Quaternion.identity);
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
